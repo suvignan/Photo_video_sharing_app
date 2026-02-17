5e1edc66b1354c6ae8bfb17d65d4a07948f9ba1f
suvignan <srisuvignan@gmail.com>
2026-02-17 18:17:21 +0530

    pre database

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,HTTPException
+from src.scheams import PostCreate,PostResponse
 
 app =FastAPI()
 
@@ -20,11 +21,18 @@ def get_all_posts(limit:int=None):
 
 @app.get('/posts/{id}')
 
-def get_post(id:int):
+def get_post(id:int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404,detail="Post Not Found")
     return text_posts.get(id)
 
 @app.post("/posts")
-def create_post():
+def create_post(post:PostCreate)->PostResponse:
+    new_post={"title":post.title,"content":post.content}
+    text_posts[max(text_posts.keys())+1]=new_post
+    return new_post
+
+@app.delete("/post")
+
+def delete_post():
     pass
